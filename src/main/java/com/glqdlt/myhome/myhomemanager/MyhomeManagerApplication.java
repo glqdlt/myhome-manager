35caf976fe48c54cad195dd9ea156eb2f9d92b24
iw.jhun <dlfdnd0725@gmail.com>
2018-07-06 23:22:20 +0900

    init

---
+++ src/main/java/com/glqdlt/myhome/myhomemanager/MyhomeManagerApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.myhome.myhomemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhomeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyhomeManagerApplication.class, args);
+    }
+}

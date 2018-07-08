bdcdef8e9e89eedef6d64611eb2895801307efa4
iw.jhun <dlfdnd0725@gmail.com>
2018-07-08 15:32:25 +0900

    webjar 리소스 경로를 assets 으로 매핑

---
+++ src/main/java/com/glqdlt/myhome/myhomemanager/webapp/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.glqdlt.myhome.myhomemanager.webapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/my-datatable/**").addResourceLocations("/webjars/my-datatable/1.0.1/");
+        registry.addResourceHandler("/assets/bootstrap/**").addResourceLocations("/webjars/bootstrap/4.1.0/");
+        registry.addResourceHandler("/assets/momentjs/**").addResourceLocations("/webjars/momentjs/2.15.1/");
+    }
+}

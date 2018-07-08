66b9650490c01c3d24d48c3ef17e1d33afddd314
iw.jhun <dlfdnd0725@gmail.com>
2018-07-08 19:47:01 +0900

    security 주석

--- src/main/java/com/glqdlt/myhome/myhomemanager/security/CustomPasswordEncoder.java
+++ src/main/java/com/glqdlt/myhome/myhomemanager/security/CustomPasswordEncoder.java
@@ -1,20 +1,20 @@
 package com.glqdlt.myhome.myhomemanager.security;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-@Slf4j
-public class CustomPasswordEncoder implements PasswordEncoder {
-
-    @Override
-    public String encode(CharSequence rawPw) {
-        log.info("lawPw : {}", rawPw.toString());
-        return rawPw.toString();
-    }
-
-    @Override
-    public boolean matches(CharSequence rawPw, String encodePw) {
-        log.info("rawPw : {} , encodedPassword : {}",rawPw.toString(), encodePw);
-        return encodePw.equals(rawPw.toString());
-    }
-}
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Slf4j
+//public class CustomPasswordEncoder implements PasswordEncoder {
+//
+//    @Override
+//    public String encode(CharSequence rawPw) {
+//        log.info("lawPw : {}", rawPw.toString());
+//        return rawPw.toString();
+//    }
+//
+//    @Override
+//    public boolean matches(CharSequence rawPw, String encodePw) {
+//        log.info("rawPw : {} , encodedPassword : {}",rawPw.toString(), encodePw);
+//        return encodePw.equals(rawPw.toString());
+//    }
+//}
--- src/main/java/com/glqdlt/myhome/myhomemanager/security/LoginUserDetail.java
+++ src/main/java/com/glqdlt/myhome/myhomemanager/security/LoginUserDetail.java
@@ -1,12 +1,12 @@
 package com.glqdlt.myhome.myhomemanager.security;
-
-import com.glqdlt.myhome.myhomemanager.persistence.member.entity.Member;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-
-public class LoginUserDetail extends User {
-
-    public LoginUserDetail(Member member) {
-        super(member.getId(), member.getPassword(), AuthorityUtils.createAuthorityList("ADMIN"));
-    }
-}
+//
+//import com.glqdlt.myhome.myhomemanager.persistence.member.entity.Member;
+//import org.springframework.security.core.authority.AuthorityUtils;
+//import org.springframework.security.core.userdetails.User;
+//
+//public class LoginUserDetail extends User {
+//
+//    public LoginUserDetail(Member member) {
+//        super(member.getId(), member.getPassword(), AuthorityUtils.createAuthorityList("ADMIN"));
+//    }
+//}

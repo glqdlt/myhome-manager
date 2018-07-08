709c5d4cd1bef92d4f3431fcc00f103b2572b212
iw.jhun <dlfdnd0725@gmail.com>
2018-07-08 19:47:10 +0900

    sb-admin 추가

--- src/main/java/com/glqdlt/myhome/myhomemanager/webapp/config/WebConfig.java
+++ src/main/java/com/glqdlt/myhome/myhomemanager/webapp/config/WebConfig.java
@@ -9,8 +9,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/assets/my-datatable/**").addResourceLocations("/webjars/my-datatable/1.0.1/");
+        registry.addResourceHandler("/assets/datatable/**").addResourceLocations("/webjars/my-datatable/1.0.1/");
         registry.addResourceHandler("/assets/bootstrap/**").addResourceLocations("/webjars/bootstrap/4.1.0/");
         registry.addResourceHandler("/assets/momentjs/**").addResourceLocations("/webjars/momentjs/2.15.1/");
+        registry.addResourceHandler("/assets/sbadmin/**").addResourceLocations("classpath:/static/sb-admin/");
     }
 }

66f3354aab91268c4b642b309f4fda31486ca4ac
iw.jhun <dlfdnd0725@gmail.com>
2018-07-10 22:43:09 +0900

    upload 기능까지 구현

--- src/main/java/com/glqdlt/myhome/myhomemanager/webapp/controller/api/book/BookRestController.java
+++ src/main/java/com/glqdlt/myhome/myhomemanager/webapp/controller/api/book/BookRestController.java
@@ -4,13 +4,14 @@ package com.glqdlt.myhome.myhomemanager.webapp.controller.api.book;
 import com.glqdlt.myhome.myhomemanager.persistence.book.entity.Book;
 import com.glqdlt.myhome.myhomemanager.service.book.BookService;
 import com.glqdlt.myhome.myhomemanager.webapp.controller.api.DataTableModel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
+@Slf4j
 // API endpoint 에 관한 좋은 이야기 => https://spoqa.github.io/2012/02/27/rest-introduction.html
 @RequestMapping("/api/books")
 @RestController
@@ -30,8 +31,29 @@ public class BookRestController {
     }
 
     @PostMapping()
-    public ResponseEntity saveBook(@RequestBody Book book) {
-        bookService.saveBook(book);
+//    public ResponseEntity saveBook(@RequestBody Book book) {
+    public ResponseEntity saveBook(
+            @RequestParam(value = "title") String title,
+            @RequestParam(value = "author") String author,
+            @RequestParam(value = "bookType") BookType bookType,
+            @RequestParam(value = "expireDate", required = false) String expireDate,
+            @RequestParam(value = "descriptionUrl", required = false) String descriptionUrl,
+            @RequestParam(value = "thumbnailUrl", required = false) String thumbnailUrl,
+            @RequestParam(value = "uploadFile", required = false) MultipartFile file,
+            @RequestParam(value = "tags", required = false) String tags) {
+
+        ScanBookDto scanBookDto = new ScanBookDto();
+        scanBookDto.setBookType(bookType);
+        scanBookDto.setTitle(title);
+        scanBookDto.setAuthor(author);
+        scanBookDto.setUploadFile(file);
+        scanBookDto.setDescriptionUrl(descriptionUrl);
+        scanBookDto.setThumbnailUrl(thumbnailUrl);
+        scanBookDto.setExpireDate(expireDate);
+        scanBookDto.setTags(tags);
+        log.info(scanBookDto.toString());
+
+        bookService.saveBook(scanBookDto);
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
---
+++ src/main/java/com/glqdlt/myhome/myhomemanager/webapp/controller/api/book/BookType.java
@@ -0,0 +1,5 @@
+package com.glqdlt.myhome.myhomemanager.webapp.controller.api.book;
+
+public enum BookType {
+    SCAN,RIDI
+}

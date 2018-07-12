6560fe3e8d8d7c0c1d45d7dc8ef15570bf25db09
iw.jhun <dlfdnd0725@gmail.com>
2018-07-12 20:12:47 +0900

    init data 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,8 +7,8 @@ INSERT INTO `tbl_tag` (`seq`, `value`) VALUES
 	(2, '프로그래밍'),
 	(3, '철학'),
 	(4, '마인드');
-INSERT INTO `tbl_book` (`archive_type`, `seq`, `description_url`, `expire_date`,  `title`, `author_seq`, `register_seq`) VALUES
-	('S', 1, NULL, NULL, '클린 코더', 1, 1);
+INSERT INTO `tbl_book` (`archive_type`, `seq`, `description_url`, `expire_date`,  `title`, `author_seq`, `register_seq`, `thumbnail_url`) VALUES
+	('S', 1, 'https://book.naver.com/bookdb/book_detail.nhn?bid=10771065', NULL, '클린 코더', 1, 1,'https://bookthumb-phinf.pstatic.net/cover/107/710/10771065.jpg');
 INSERT INTO `tbl_book_tags` (`tbl_book_seq`, `tags_seq`) VALUES
 	(1, 1),
 	(1, 2);

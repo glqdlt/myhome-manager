f737c1facbe39d3f1ba8b0003e0614e76d8c823b
전일웅 <jhun@nm-4ones.com>
2018-07-12 17:28:07 +0900

    쿠폰 추가

---
+++ src/main/java/com/glqdlt/myhome/myhomemanager/persistence/coupon/entity/EventCouponSerial.java
@@ -0,0 +1,12 @@
+package com.glqdlt.myhome.myhomemanager.persistence.coupon.entity;
+
+
+import lombok.Data;
+
+@Data
+public class EventCouponSerial {
+
+    private EventCoupon eventCoupon;
+    private Integer couponIssueCount;
+
+}

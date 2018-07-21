c8570ecc352d1dda387765c7b0475dc4e534617c
iw.jhun <dlfdnd0725@gmail.com>
2018-07-21 10:44:34 +0900

    파워매니저 모듈 추가

---
+++ server-power-manager/src/test/java/com/glqdlt/serverpowermanager/ServerPowerManagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.glqdlt.serverpowermanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerPowerManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

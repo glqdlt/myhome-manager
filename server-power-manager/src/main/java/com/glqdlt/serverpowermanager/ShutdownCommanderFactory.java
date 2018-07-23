93fa6415199279650d57316fed77eba2a2f20377
iw.jhun <dlfdnd0725@gmail.com>
2018-07-23 21:29:19 +0900

    누락파일

---
+++ server-power-manager/src/main/java/com/glqdlt/serverpowermanager/ShutdownCommanderFactory.java
@@ -0,0 +1,14 @@
+package com.glqdlt.serverpowermanager;
+
+public class ShutdownCommanderFactory {
+    public static ShutdownCommander generate(String command){
+        if (command.toUpperCase().equals(ShutdownCommander.SHUTDOWN.name())) {
+            return ShutdownCommander.SHUTDOWN;
+        } else if (command.toUpperCase().equals(ShutdownCommander.RESTART.name())) {
+            return ShutdownCommander.RESTART;
+        } else if (command.toUpperCase().equals(ShutdownCommander.WAKE.name())) {
+            return ShutdownCommander.WAKE;
+        }
+        return null;
+    }
+}
